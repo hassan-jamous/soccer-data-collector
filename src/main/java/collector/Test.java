b52c47047349c765539d44a2bc017faa4a7779eb
MohammadAlloush1985 <Mohammad.alloush1970@gmail.com>
2020-12-09 15:32:26 +0200

    first commit alloush

--- src/main/java/collector/Test.java
+++ src/main/java/collector/Test.java
@@ -2,6 +2,14 @@ package collector;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello From Hassan");
+        ScreenScraper testObject = new ScreenScraper();
+        
+        //test
+       //testObject.getAllGamesOfLeague("","2015-2016"); test again with other years
+        String ownGoal = "https://www.worldfootball.net/report/premier-league-2015-2016-arsenal-fc-aston-villa/";
+        String individuallyGoal ="https://www.worldfootball.net/report/premier-league-2015-2016-sunderland-afc-chelsea-fc/";
+        testObject.getEventsOfGame(individuallyGoal);
+        System.out.println("End Main");
     }
 }
