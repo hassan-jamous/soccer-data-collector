cab91cf2db28504f7d23decf6c1b86cf66f7a402
Your Name <mohammad.alloush1966@gmail.com>
2021-02-14 00:18:09 +0200

    laliga's statistics since 2005/2006 to 2020/2021

--- src/test/java/sofaScore/SeasonCollectorTest.java
+++ src/test/java/sofaScore/SeasonCollectorTest.java
@@ -28,7 +28,7 @@ public class SeasonCollectorTest {
 		//seasonCollectorTest.writeCompetitionBasicInfoAndStatistics("LaLiga", "2013/2014", "2014/2015");
 		//seasonCollectorTest.writeCompetitionBasicInfoAndStatistics("LaLiga", "2015/2016", "2016/2017");
 		//seasonCollectorTest.writeCompetitionBasicInfoAndStatistics("LaLiga", "2017/2018", "2018/2019");
-		//seasonCollectorTest.writeCompetitionBasicInfoAndStatistics("LaLiga", "2019/2020", "2020/2021");
+		seasonCollectorTest.writeCompetitionBasicInfoAndStatistics("LaLiga", "2019/2020", "2020/2021");
 	
 		//seasonCollectorTest.writeCompetitionBasicInfoAndStatistics("Bundesliga", "2005/2006", "2006/2007");
 		//seasonCollectorTest.writeCompetitionBasicInfoAndStatistics("Bundesliga", "2007/2008", "2008/2009");
@@ -64,6 +64,6 @@ public class SeasonCollectorTest {
 	public void itShouldWriteSeasonIncidents() {
 		
 		//seasonCollectorTest.roundCollerctor.writeRoundIncidents("Premier League", "2019/2020", "1", "");
-		seasonCollectorTest.writeSeasonIncidents("Premier League", "2019/2020");
+		//seasonCollectorTest.writeSeasonIncidents("Premier League", "2019/2020");
 	}	
 }
